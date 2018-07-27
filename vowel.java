09d6c7a6fb1b8ff4d166553e96486add2f12dfea
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-07-26 23:01:40 -0700

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -5,7 +5,7 @@ public static void main(String[] args)
 char ch;
 System.out.println("enter the character");
 Scanner sc=new Scanner(System.in);
-ch=sc.nextChar();
+ch=sc.next().charAt(0);
 if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
 System.out.println("given character is vowel");
 else
