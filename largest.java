7bb510f74c211e55cb6d6d9ece69011ba75016f5
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-07-26 23:54:46 -0700

    Update largest.java

--- largest.java
+++ largest.java
@@ -3,7 +3,6 @@ public class Demo
 public static void main(String args[])
 {
 int x,y,z;
-System.out.println("enter three integers");
 Scanner sc=new Scanner(System.in);
 x=sc.nextInt();
 y=sc.nextInt();
@@ -14,8 +13,6 @@ else if(y>x&&y>z)
 System.out.println(y);
 else if(z>x&&z>y)
 System.out.println(z);
-else 
-System.out.println("three numbers are distinct");
 }
 }
 
