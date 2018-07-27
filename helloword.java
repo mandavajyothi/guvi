8592520f3983cb246efd9fe2f6deeed80d8cb025
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-07-27 04:22:40 -0700

    Create helloword.java

---
+++ helloword.java
@@ -0,0 +1,13 @@
+public class Demo
+{
+public static void main(String args[])
+{
+BufferedReader br=new BufferedReader(new  InputStreamReader(System.in));
+String line=br.readLine();
+int N=Integer.parseInt(line);
+for(int i=0;i<N;i++)
+{
+System.out.println("hello word");
+}
+}
+}
