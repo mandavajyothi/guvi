d23e634027d3183104d565d7f896354dcb6d5ce5
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-07-26 22:20:28 -0700

    Update even.java

--- even.java
+++ even.java
@@ -3,7 +3,7 @@ public class Demo
 public static void main(String[] args)
 {
 Scanner sc=new Scanner(System.in);
-int number;
+longint number;
 System.out.println("enter the number");
 number=sc.nextInt();
 if(number%2==0)
