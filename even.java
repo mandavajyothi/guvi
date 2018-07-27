59b70549f53edfa23b5170de941e43c74a03490a
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-07-26 22:21:57 -0700

    Update even.java

--- even.java
+++ even.java
@@ -5,7 +5,7 @@ public static void main(String[] args)
 Scanner sc=new Scanner(System.in);
 longint number;
 System.out.println("enter the number");
-number=sc.nextInt();
+number=sc.nextLongInt();
 if(number%2==0)
 System.out.println("given number is even");
 else 
