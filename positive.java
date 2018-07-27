9205c8ad0f2be382be149cd3a1dd1f9f9f0e64f7
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-07-26 22:01:46 -0700

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,16 @@
+public class Demo
+{
+public stsatic void main(String[] args)
+{
+Scanner sc=new Scannner(System.in);
+int number;
+System.out.println("enter a number");
+number=sc.newInt();
+if(number>0)
+System.out.println("given number is positive");
+else if(number<0)
+System.out.println("given number is negative");
+else
+System.out.println("given number is zero");
+}
+}
