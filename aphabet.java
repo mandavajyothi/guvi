38a1b175bec40821ceeb2ecccaa717621149ecce
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-07-26 23:23:38 -0700

    Update aphabet.java

--- aphabet.java
+++ aphabet.java
@@ -4,9 +4,9 @@ public static void main(String[] args)
 {
 char  c='a';
 if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
-System.out.println(c + "is an alphabet");
+System.out.println("Alphabet");
 else
-System.out.println(c +"is not an alphabet");
+System.out.println("no");
 }
 }
 
