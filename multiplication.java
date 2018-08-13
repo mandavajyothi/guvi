765fd549232867d2cc1360bc4189a5fe1779b5a0
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-13 12:40:34 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,11 @@
+class Multiplication
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(int c=1;c<=5;c++)
+{
+System.out.println((n+c)+" ");
+}
+}
