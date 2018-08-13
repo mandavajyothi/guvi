03fb8d69bc8145de75d0c8d84e66b9a859b00636
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-13 12:59:11 +0530

    Create armstrongprintyesorno.java

---
+++ armstrongprintyesorno.java
@@ -0,0 +1,23 @@
+public class Demo
+{
+public static void main(String args[])
+{
+int c=0,a,temp;
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+temp=n;
+while(n>0)
+{
+a=n%10;
+n=n/10;
+c=c+(a*a*a);
+}
+if(temp==c)
+{
+System.out.println("yes");
+}
+else
+System.out.println("no");
+}
+}
