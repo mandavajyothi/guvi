c0386957672c8006f5805a97c1400494da6fc62f
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-14 11:54:05 +0530

    Create primeyesorno.java

---
+++ primeyesorno.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Demo
+{
+public static void main(string args[])
+{
+int num;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+boolean flag=false;
+for(int i=2;i<=num/2;++i)
+{
+if(num%i==0)
+{
+flag=true;
+break;
+}
+}
+if(!flag)
+System.out.println("yes");
+else
+system.out.println("no");
+}
+}
