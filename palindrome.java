6c8077843ae7fcf07ddb987d11cb04ef81e487da
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-14 11:30:34 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Palindrome
+{
+public static void main(String args[])
+{
+int num,reversedInteger=0,remainder,originalInteger;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+orginalInteger=num;
+while(num!=0)
+{
+remainder=num%10;
+reversedInteger=reversedInteger*10+remainder;
+num/=10;
+}
+if(originalInteger==reversedInteger)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
+
