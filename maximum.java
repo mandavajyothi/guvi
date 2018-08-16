af5a2acc9fb4af772614e9687106b2e076f86ab1
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-16 12:43:26 +0530

    Create maximum.java

---
+++ maximum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Maximum
+{
+public static void main(String args[])
+{
+int large=0;
+int num;
+Scanner input=new Scanner(System.in);
+int n=input.nextInt();
+num=input.nextInt();
+for(int i=2;i<n;i++)
+{
+num=input.nextInt();
+if(num>large)
+{
+large=num;
+}
+}
+System.out.println(large);
+}
+}
