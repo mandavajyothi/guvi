c5e3a1fbd41970b458e8b47b06b0c9c529b4c925
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-16 11:59:05 +0530

    Update specialcharacter.java

--- specialcharacter.java
+++ specialcharacter.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.*;
-import java.io.*;'
-import java.io.Bufferedreader;
+import java.io.*;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 class SpecialCharacter
@@ -10,7 +10,7 @@ public static String password="";
 public static final String FINAL_CHAR_REGEX="[!@#$%^&*()[\\\\]|;',./{}\\\\\\\\:\\\"<>?]";
 public static void main(String args[])throws IOException
 {
-Bufferedreader dataIn=new Bufferedreader(new InputStreamReader(System.in));
+BufferedReader dataIn=new BufferedReader(new InputStreamReader(System.in));
 password=dataIn.readLine();
 for(int i=0;i<password.length();i++)
 {
