8ce0022d2852cced8aab9c95b9644ae8fd1dbfb0
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-16 13:05:13 +0530

    Update wordcount.java

--- wordcount.java
+++ wordcount.java
@@ -3,13 +3,13 @@ class Wordcount
 {
 public static void main(String args[])
 {
-Scanner sc=new scanner(system.in);
+Scanner sc=new Scanner(System.in);
 String s;
 s=sc.nextLine();
 int count=1;
 for(int i=0;i<s.length()-1;i++)
 {
-if((s.charAt(i)==' ')&&(s.charAt(i+1)!==' '))
+if((s.charAt(i)==' ')&&(s.charAt(i+1)!=' '))
 {
 count++;
 }
