c078aaf32b53eea6dee13ab98b26ba9ac957a1fa
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-22 10:23:31 +0530

    Create concatenatestrings.java

---
+++ concatenatestrings.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class ConcatenateStrings
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String firststr=sc.next();
+String secondstr=sc.next();
+System.out.println(firststr+""secondstr);
+}
+
