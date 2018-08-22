1985cc04403796869821705e6e59b6cffe2b66a6
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-22 10:26:46 +0530

    Update concatenatestrings.java

--- concatenatestrings.java
+++ concatenatestrings.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 class ConcatenateStrings
+{
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 String firststr=sc.next();
 String secondstr=sc.next();
-System.out.println(firststr+""secondstr);
+System.out.println(firststr+""+secondstr);
+}
 }
 
