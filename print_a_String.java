fc5c40ba3010e392d7dd757fe8494e7710207d69
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-23 15:25:35 +0530

    Create print_a_String.java

---
+++ print_a_String.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class printaString
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextInt();
+int n=sc.nextInt();
+for(int i=0;i<=n;i++)
+{
+System.out.println(s);
+}
+}
