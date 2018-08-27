441f7286a9f35e2f42ec8cf73ad147f414042577
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-27 10:48:57 +0530

    Create maximum_number.java

---
+++ maximum_number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class MaximumNumber
+{
+public static void main(String args[])
+{
+int n,max;
+Scanner s=new Scanner (System.in);
+int a[]=new int[10];
+for(int i=0;i<10;i++)
+{
+a[i]=s.nextInt();
+}
+max=a[0];
+for(int i=0;i<10;i++)
+{
+if(max<a[i])
+{
+max=a[i];
+}
+}
+System.out.println(max);
+}
+}
+
