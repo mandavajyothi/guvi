937a0d7ef2ba991e0425761d7ed0779118be2c35
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-27 16:01:08 +0530

    Create hours_minutes.java

---
+++ hours_minutes.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+class Hours_minutes
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int t=sc.nextInt();
+int hours=t/60;
+int minutes=t%60;
+System.out.println(hours+" "+minutes);
+}
+}
+
+
+
