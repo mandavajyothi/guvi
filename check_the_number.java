29e762a524faab9e60ef4a0b7b53678fe7f37b35
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-28 10:07:27 +0530

    Create check_the_number.java

---
+++ check_the_number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Check_the_number
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>=1&&n<=10)
+{
+System.out.println("yes");
+}
+else
+System.out.println("no");
+}
+}
