e4525d6da2e555e31d5ee307a83bf0478f0ea07a
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-28 16:14:56 +0530

    Create all_digitsInSameOrder.java

---
+++ all_digitsInSameOrder.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class AllDigitsInSameOrder
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str[]=sc.nextLine(3);
+
+for(int i=0;i<3;i++)
+{
+str[i]=sc.nextLine();
+}
+for(int j=0;j<3;j++)
+{
+System.out.println(str);
+}
+}
+}
