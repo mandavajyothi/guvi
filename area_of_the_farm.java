adbdbddddc4be4f2f06f1318277cdc2bee058f8c
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-29 14:39:41 +0530

    Create area_of_the_farm.java

---
+++ area_of_the_farm.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+class AreaOfTheFarm
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		double c;
+		c=a*b;
+	  System.out.format("%.5f", c);	
+		
+	}
+}
