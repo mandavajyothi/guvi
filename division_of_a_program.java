f8ec6524233a8a59a5da67d86836369d241878b1
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-29 15:11:52 +0530

    Create division_of_a_program.java

---
+++ division_of_a_program.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Subtarction
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		long a=sc.nextLong();
+		long b=sc.nextLong();
+		long c;
+		c=a/b;
+		System.out.println(c);
+	}
+}
