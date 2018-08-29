b37b70f141dc1d5452f42c7ae40c65387a4d4b82
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-29 10:06:44 +0530

    Create perfect_sqare.java

---
+++ perfect_sqare.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Perfect_Square {
+     
+    static boolean isPerfectSquare(double x) 
+    {
+        double sr = Math.sqrt(x);
+        return ((sr - Math.floor(sr)) == 0);
+    }
+     
+   
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int x;
+        x=n*m;
+         
+        if (isPerfectSquare(x))
+            System.out.print("yes");
+        else
+            System.out.print("no");
+    }
+}
+ 
