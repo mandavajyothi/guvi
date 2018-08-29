552783918daa63be2eb7eebbfb8ab9bd611dc1e1
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-29 14:46:19 +0530

    Update area_of_the_farm.java

--- area_of_the_farm.java
+++ area_of_the_farm.java
@@ -7,8 +7,8 @@ class AreaOfTheFarm
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		int b=sc.nextInt();
+		double a=sc.nextDouble();
+		double b=sc.nextDouble();
 		double c;
 		c=a*b;
 	  System.out.format("%.5f", c);	
