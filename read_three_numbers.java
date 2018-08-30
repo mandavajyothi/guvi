f379fbbf63cfa4af977b1098d4192596c7970b74
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-30 14:29:58 +0530

    Create read_three_numbers.java

---
+++ read_three_numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Three_numbers
+{
+public static void main(String args[])
+{
+int a,b,c,res;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+c=sc.nextInt();
+res=(a*b)/c;
+System.out.println(res);
+}
+}
