d5f63837f486e6bfcc61c9f9fb047358295ca18d
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-30 11:01:58 +0530

    Create number_of_digits.java

---
+++ number_of_digits.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class NumberOfDIgits
+{
+public static void main(String args[])
+{
+int count=0,num;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+while(num!=0)
+{
+num/=10;
+++count;
+}
+System.out.println(count);
+}
+}
+
