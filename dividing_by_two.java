e86840440c0fd6ed2411a6d2eaa06118030bdd27
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-08-30 16:01:22 +0530

    Create dividing_by_two.java

---
+++ dividing_by_two.java
@@ -0,0 +1,20 @@
+ import java.util.*;
+ class DividingByTwo 
+ {
+ public static void main(String[] args) 
+    {
+        int num,res,ans;
+        Scanner sc=new Scanner(System.in);
+        num=sc.nextInt();
+        if((num%2)==0)
+        {
+        ans=num/2;
+        System.out.println(ans);
+        }
+        else
+        System.out.println(num);
+        }
+     
+        }
+        
+ 
