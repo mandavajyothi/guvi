72735d31c47c8f082228bf343854bb2c586af6a1
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-01 16:02:38 +0530

    Create small_number.java

---
+++ small_number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+class Small_number
+{
+public static void main(String args[])
+{
+int smallest=0;
+int large=0;
+int num;
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+num=sc.nextInt();
+smallest=num;
+for(int i=0;i<n;i++)
+{
+num=sc.nextInt();
+if(num<smallest)
+{
+smallest=num;
+}
+}
+System.out.println(smallest);
+}
+}
+
