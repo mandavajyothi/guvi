ed58576050e45c17adc6f11b866e3ee98838fd8d
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-01 15:18:24 +0530

    Create first_three.java

---
+++ first_three.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class First_Three
+{
+public static void main(String args[])
+{
+String str;
+Scanner sc=new Scanner(System.in);
+str=sc.nextLine();
+System.out.println(str.substring(0,3));
+}
+}
+
