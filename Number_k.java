f6bf068570ff7e2855ee4e5c0930aeb005050096
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-04 12:28:35 +0530

    Create Number_k.java

---
+++ Number_k.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class Number_k
+{
+public static void main(String args[])
+{
+int n,k;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+k=sc.nextInt();
+System.out.println(k);
+}
+}
+
