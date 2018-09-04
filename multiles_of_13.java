b62fad54ba1a45d2a9387995df108ebd074703b7
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-04 10:14:31 +0530

    Update multiles_of_13.java

--- multiles_of_13.java
+++ multiles_of_13.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Multiples 
 {
-    public static void main(String[]args) 
+    public static void main(String args[]) 
     {   
         Scanner sc=new Scanner(System.in); 
         int j=sc.nextInt();
