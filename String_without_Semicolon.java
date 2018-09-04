ea9dc70c25849752f57381d487cea75c51bdce74
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-04 10:53:57 +0530

    Create String_without_Semicolon.java

---
+++ String_without_Semicolon.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class String_without_semicolon
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s;
+s=sc.nextLine();
+if(System.out.printf(s)!=null){ }
+}
+}
