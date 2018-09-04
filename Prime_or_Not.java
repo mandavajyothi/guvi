00cdb6371673a122626009794637d7cfbb93fce5
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-04 11:01:18 +0530

    Create Prime_or_Not.java

---
+++ Prime_or_Not.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Prime_OR_not
+{
+public static void main(String args[])
+{
+int num;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+boolean flag=false;
+for(int i=2;i<=num/2;++i)
+{
+if(num%i==0)
+{
+flag=true;
+break;
+}
+}
+if(!flag)
+System.out.println("yes");
+else
+System.out.println("no");
+}
+}
