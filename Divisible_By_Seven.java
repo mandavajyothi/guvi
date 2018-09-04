e3260a85d210d2fe3abd4d1beb5e5cea80f741d7
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-04 11:10:20 +0530

    Create Divisible_By_Seven.java

---
+++ Divisible_By_Seven.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+class Divisible_by_seven
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int i=sc.nextInt();
+if(i%7==0)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
+
