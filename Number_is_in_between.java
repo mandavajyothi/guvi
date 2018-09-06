6c7a8d97b599ba0dc83ce6d5843525e07cbe7658
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-06 12:39:21 +0530

    Create Number_is_in_between.java

---
+++ Number_is_in_between.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.io.*;
+class Number_is_In_between
+{
+public static void main(String args[])
+{
+int n,l,r;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+l=sc.nextInt();
+r=sc.nextInt();
+if(l<=n<=r)
+{
+
+System.out.println("yes");
+}
+else
+System.out.println("no")
+}
+}
