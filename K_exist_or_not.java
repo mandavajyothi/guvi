af79880a609d5ea29cf3285170c66ee9f6e6f308
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-06 12:06:45 +0530

    Create K_exist_or_not.java

---
+++ K_exist_or_not.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+class K_exist_or_not
+{
+public static void main(String args[])
+{
+int n,k,i;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+k=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+if(a[i]==k)
+{
+System.out.println("yes");
+break;
+}
+}
+}
+}
+
