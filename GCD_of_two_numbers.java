152c2b68bd6137fccdb452207500fbb14a6f806a
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-06 14:25:36 +0530

    Create GCD_of_two_numbers.java

---
+++ GCD_of_two_numbers.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class GCD 
+{
+
+    public static void main(String[] args)
+    {
+
+        int n1, n2,gcd = 1;
+        Scanner sc=new Scanner(System.in);
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        
+        for(int i = 1; i <= n1 && i <= n2; ++i)
+        {
+          
+            if(n1 % i==0 && n2 % i==0)
+                gcd = i;
+        }
+
+        System.out.println(gcd);
+    }
+}
