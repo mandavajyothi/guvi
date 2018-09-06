42070e787d4c0dccdb295ac1047431448b1d6b03
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-06 09:40:31 +0530

    Create Print_median_element.java

---
+++ Print_median_element.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+class Median_element
+{
+public static void main(String args[])
+{
+int n,sum=0,res;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+sum=a[i]+sum;
+}
+res=sum/n;
+System.out.println(res);
+}
+}
