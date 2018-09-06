84f6ca756b366040956970399c8c89436034153b
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-06 09:55:01 +0530

    Create Print_element_with_Index.java

---
+++ Print_element_with_Index.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+{
+class Element_with_index
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(system.in);
+n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+System.out.println(a[i]+" "+i);
+}
+}
+}
+
