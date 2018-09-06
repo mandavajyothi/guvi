e784bf7822258c1b832280935182a86d20034982
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-06 10:42:53 +0530

    Create Arithmetic_progression.java

---
+++ Arithmetic_progression.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class Arithmetic_progression
+{
+public static void main(String args[])
+{
+int n,a,b;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+a=sc.nextInt();
+b=sc.nextInt();
+System.out.println((n*((2*a)+(n-1)*d))/2);
+}
+}
