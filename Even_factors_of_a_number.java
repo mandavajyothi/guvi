cb4f38e3a76f234c7bf79db64a05dbde5a7bce13
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-07 12:37:36 +0530

    Create Even_factors_of_a_number.java

---
+++ Even_factors_of_a_number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Even_factors 
+{
+public static void main(String[] args) 
+{
+    Scanner sc=new Scanner(System.in);
+        int number = sc.nextInt();
+        for(int i = 2; i <= number; ++i) 
+        {
+            if (number % i == 0) 
+            {
+            	if(i%2==0)
+            	{
+           
+                            System.out.printf(i+" ");
+                        }
+            }
+        }
+    }
+}
