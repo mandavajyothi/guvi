ac87d287504521648d4a4844899525f6707fa223
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-07 12:25:16 +0530

    Create Vishal.java

---
+++ Vishal.java
@@ -0,0 +1,24 @@
+
+import java util.*;
+import java.io.*;
+class Vishal
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n*2];
+for(int i=0;i<(n*2);i++)
+{
+a[i]=sc.nextInt();
+}
+if(n%2==0)
+{
+System.out.println(n-1);
+}
+else
+{
+System.out.println(n-2);
+}
+}
+}
