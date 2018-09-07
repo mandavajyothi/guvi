b389c287e4ebf1107bcde18788bd3cad9a3ad7d5
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-07 16:09:44 +0530

    Update all_digitsInSameOrder.java

--- all_digitsInSameOrder.java
+++ all_digitsInSameOrder.java
@@ -4,7 +4,7 @@ class All_digits_SameOrder
   public static void main(String args[])
   {
     Scanner sc=new Scanner(System.in);
-    int number=in.nextInt();
+    int number=sc.nextInt();
     int tall=1;
     for(int t=0;tall<=3;tall++)
     {
