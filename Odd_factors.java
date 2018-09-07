b29a76f7e493c38a35fc4534451b5beb2fdb4381
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-07 15:20:07 +0530

    Create Odd_factors.java

---
+++ Odd_factors.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Odd_factors 
+{
+public static void main(String[] args) 
+{
+    Scanner sc=new Scanner(System.in);
+        int number = sc.nextInt();
+        for(int i = 1; i <=number; ++i) 
+        {
+            if (number % i == 0) 
+            {
+            	if(i%2!=0)
+            	{
+           
+                            System.out.printf(i+" ");
+                        }
+            }
+        }
+    }
+}
