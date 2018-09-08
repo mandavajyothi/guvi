2545711451bed7bb45866f96f4e490172406007c
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-08 10:27:48 +0530

    Create String_is_isogram_or_not.java

---
+++ String_is_isogram_or_not.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class String_is_isogram_or_not
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+isIsogram(str);
+}
+static void isIsogram(String s)
+{
+String[] ary=s.split("");
+Set<String> mySet=new HashSet<String>(Arrays.asList(ary));
+if(s.length()==mySet.size())
+{
+System.out.println("Yes");
+}
+else
+{
+System.out.println("No");
+}
+}
+}
+
