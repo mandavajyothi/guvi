9dde15e952a0988b88a9442214c8494b1f1e19cd
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-08 09:37:43 +0530

    Create Number_is_between_L_and_R.java

---
+++ Number_is_between_L_and_R.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Number_is_between_L_and_R
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in)
+int n=sc.nextInt();
+int l=sc.nextInt();
+int r=sc.nextInt();
+if(n>l && n<r)
+System.out.println("yes");
+else
+System.out.println("no");
+}
+}
+
