d8bd5eb38dc72b7a6d70e8166ac86b9febb23ec9
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-08 09:45:42 +0530

    Create Nearest_greater_integer.java

---
+++ Nearest_greater_integer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Nearest_greater_integer
+{
+public static void main(string args[])
+{
+Scanner sc=new Scanner(System.in);
+double d=sc.nextDouble();
+System.out.println((int)Math.ceil(d));
+}
+}
+
