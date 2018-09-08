ba5426232bcbb641e539df1f7b98b65c1a679993
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-08 11:40:15 +0530

    Update Total_area_and_volume_of_a_cuboid.java

--- Total_area_and_volume_of_a_cuboid.java
+++ Total_area_and_volume_of_a_cuboid.java
@@ -1,8 +1,6 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-
 class total_area_and_volume_ofa_cuboid
 {
 	public static void main (String[] args) throws java.lang.Exception
@@ -13,11 +11,11 @@ class total_area_and_volume_ofa_cuboid
 		int h=sc.nextInt();
 		int s,v;
 		if(l==1&&b==1&&h==1)
-		{
+		 {
 			s=2*l*b*h;
 			v=l*b*h;
 			System.out.print(s+" "+v);
-		}
+		 }
 		else
 		{
 			s=((2*l*b)+(2*b*h)+(2*h*l));
