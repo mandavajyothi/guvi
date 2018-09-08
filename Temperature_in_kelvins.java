897fb9eeeea0368fde0c06683ac8b9b393d66ab0
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-08 11:44:54 +0530

    Create Temperature_in_kelvins.java

---
+++ Temperature_in_kelvins.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Temperaturein_kelvin
+{
+	public static void main (String[] args) 
+	{
+	
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m;
+		m=n+273;
+		System.out.println(m);
+		}
+}
