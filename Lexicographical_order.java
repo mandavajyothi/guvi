989dbb14f162dea40e18fd24928e218b116ec28c
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-08 10:33:56 +0530

    Create Lexicographical_order.java

---
+++ Lexicographical_order.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Lexicographical_order.java
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+char str1[]=str.toCharArray();
+Arrays.sort(str1);
+System.out.print(str1);
+}
+}
+
