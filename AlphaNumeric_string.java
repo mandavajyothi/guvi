75adfeb65ca487ff6ada81d31b5ed8322fc402e5
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-08 10:43:07 +0530

    Create AlphaNumeric_string.java

---
+++ AlphaNumeric_string.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class AplhsNumeric_string
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+for(int i=0;i<str.length();i++)
+{
+if(Character.isDigit(str.charAt(i)))
+System.out.print(str.charAt(i));
+}
+}
+}
+
