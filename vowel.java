30513ef389334e46ffd8bc29d8b3f481aab73a27
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-20 15:00:54 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -8,10 +8,8 @@ Scanner sc=new Scanner(System.in);
 ch=sc.next().charAt(0);
 if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
 System.out.println("Vowel");
-else if
+else
 System.out.println("Consonent");
-  else
-    System.out.println("invalid");
   
 }
 }
